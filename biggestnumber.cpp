42b3470cf3c35e38aa26434dea24301bf98ea3fe
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-05 17:39:34 -0800

    Adds else statement and outputs largest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,12 +7,24 @@ int main()
 {
   int first; 
   int second;
+  int biggest;
   cout<<"Please enter a whole number:\n";
   cin>> first; 
   cout<<"Please enter another whole number:\n";
   cin>> second;
+  if (first>=second)
+  {
+    biggest=first;
+  }
+  else
+  {
+    biggest=second;
+  }
   cout<<"Of those two numbers, the biggest is: ";
+  cout<< biggest;
   cout<<endl<<"Thank you for playing.\n";
-
+  
+  
+  
   return 0;
 }
